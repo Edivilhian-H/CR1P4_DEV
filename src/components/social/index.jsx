5936d06416596996fc69a7a7bd63da7f0208cf53
Edivilhian-Hipolito <97316805+Edivilhian-H@users.noreply.github.com>
2026-05-04 07:48:45 -0300

    Starting social area

---
+++ src/components/social/index.jsx
@@ -0,0 +1,20 @@
+function social() {
+  return (
+    <div className="social">
+      <a href="https://www.facebook.com/">
+        <i className="fa fa-facebook"></i>
+      </a>
+      <a href="https://www.twitter.com/">
+        <i className="fa fa-twitter"></i>
+      </a>
+      <a href="https://www.linkedin.com/">
+        <i className="fa fa-linkedin"></i>
+      </a>
+      <a href="https://www.instagram.com/">
+        <i className="fa fa-instagram"></i>
+      </a>
+    </div>
+  );
+}
+
+export default social;
