9bfaa9beeede7d59728f8563a5f2836f4412eeb9
edivilhian-H <edivilhianhipolito2005@gmail.com>
2026-07-30 20:08:55 -0300

    Finalizing the responsiveness of the social links

--- src/components/social/index.jsx
+++ src/components/social/index.jsx
@@ -3,15 +3,15 @@ import * as S from "./styles";
 function Social() {
   return (
    <S.Social>
-  <a href="#" target="_blank">
+  <a href="https://github.com/Edivilhian-H" target="_blank">
     <i className="bx bxl-github"></i>
   </a>
 
-  <a href="#" target="_blank">
+  <a href="https://br.linkedin.com/in/edivilhian-h-santos" target="_blank">
     <i className="bx bxl-linkedin"></i>
   </a>
 
-  <a href="#" target="_blank">
+  <a href="https://www.instagram.com/edivilhian.hp/" target="_blank">
     <i className="bx bxl-instagram"></i>
   </a>
 </S.Social>
